bde3efb8adfec3549f46dc74c1e2f9c33bee0a6b
Mingsheng Gan <mingsheng88@hotmail.com>
2020-03-13 17:30:05 +0800

    Support search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  def search
+    if params[:q].nil?
+      render json: { error: "'q' is a required search parameter" }
+    elsif !params[:q].is_a?(String)
+      render json: { error: "q must be a string" }
+    else
+      packages = CranPackage.where("lower(name) like ?", "%#{params[:q].downcase}%").limit(20)
+      render json: packages
+    end
+  end
 end
